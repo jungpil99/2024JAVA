b986d5b59a788fe6897d90d8dac597aa7b76b551
jungpil99 <kaka87355377@gmail.com>
2024-07-09 16:36:13 +0900

    0709예제

---
+++ java0709/src/java0709/Ex08.java
@@ -0,0 +1,25 @@
+package java0709;
+
+import java.util.Scanner;
+
+/**
+ * 입력받은 숫자가 양수일때 양수로 음수일때 양수로 바꿔라 
+ */
+public class Ex08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하시오:");
+		int a = scan.nextInt();
+		
+		if( a > 0) {
+			System.out.printf("%d", a);
+		}else {
+			int abs = Math.abs(a);
+			System.out.printf("%d", abs);
+		}
+	}
+
+}
