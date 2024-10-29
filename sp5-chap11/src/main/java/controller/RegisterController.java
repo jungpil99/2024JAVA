361f86d6c3bf85feec1a91f28eac79a493ef00c7
jungpil99 <kaka87355377@gmail.com>
2024-10-29 19:50:15 +0900

    1029

--- sp5-chap11/src/main/java/controller/RegisterController.java
+++ sp5-chap11/src/main/java/controller/RegisterController.java
@@ -1,10 +1,14 @@
 package controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -50,8 +54,8 @@ public class RegisterController {
 	}
 
 	@PostMapping("/register/step3")
-	public String handleStep3(RegisterRequest regReq, BindingResult errors) {
-		new RegisterRequestValidator().validate(regReq, errors);
+	public String handleStep3(@Valid RegisterRequest regReq, BindingResult errors) {
+//		new RegisterRequestValidator().validate(regReq, errors);
 		if(errors.hasErrors())
 			return "register/step2";
 		
@@ -61,9 +65,14 @@ public class RegisterController {
 		} catch (DuplicateMemberException ex) {
 //			errors.rejectValue("email", "duplicate");
 			errors.reject("notMatchingPassword");
-			errors.rejectValue("registerRequest", "notMatchingPassword");
+//			errors.rejectValue("registerRequest", "notMatchingPassword");
 			return "register/step2";
 		}
+	}
+	
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		binder.setValidator(new RegisterRequestValidator());
 	}
 
 }
